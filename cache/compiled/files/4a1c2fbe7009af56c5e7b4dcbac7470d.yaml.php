fb4ed726a3528d840c4f68c1fb34b517341d7250
Thomas Gredin <t.gredin@gmx.fr>
2018-10-11 15:58:09 +0200

    add missing file

---
+++ cache/compiled/files/4a1c2fbe7009af56c5e7b4dcbac7470d.yaml.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'C:/laragon/www/grav-admin/user/config/plugins/pagination.yaml',
+    'modified' => 1539019756,
+    'data' => [
+        'enabled' => true,
+        'built_in_css' => false,
+        'delta' => 0
+    ]
+];
